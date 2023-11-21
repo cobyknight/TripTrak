90f43a9add76f9a136e172838b888969a2e0f3c8
Dylan Scivicque <dylanscivicque@gmail.com>
2023-11-20 18:25:17 -0600

    added login process

--- profilepage.js
+++ profilepage.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+// ProfilePage.js
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, TextInput, Button } from 'react-native';
+import { handleSignUp, handleSignIn, handleSignOut, sendPasswordReset } from './authService'; // Import authentication handle methods
 import FontAwesome from 'react-native-vector-icons/FontAwesome'; // Import FontAwesome
 
 function Profile({ navigation }) {
-  // Example function to handle logout
-  const handleLogout = () => {
-    // Perform logout operations...
-    console.log('User logged out');
-    // Then navigate to the login screen or another appropriate screen
-    // navigation.navigate('Login');
+  const [signUpEmail, setSignUpEmail] = useState('');
+  const [signUpPassword, setSignUpPassword] = useState('');
+  const [signInEmail, setSignInEmail] = useState('');
+  const [signInPassword, setSignInPassword] = useState('');
+  const [resetPasswordEmail, setResetPasswordEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSignUpClick = () => {
+    handleSignUp(signUpEmail, signUpPassword, setMessage);
+  };
+
+  const handleSignInClick = () => {
+    handleSignIn(signInEmail, signInPassword, setMessage);
+  };
+
+  const handleSignOutClick = () => {
+    handleSignOut(setMessage);
+  };
+
+  const handleResetPasswordClick = () => {
+    sendPasswordReset(resetPasswordEmail)
+      .then(() => setMessage('Password reset email sent!'))
+      .catch((error) => setMessage(`Password reset failed: ${error.message}`));
   };
 
   // Placeholder functions for other menu items
@@ -16,31 +35,82 @@ function Profile({ navigation }) {
   const handleNotifications = () => console.log('Navigate to Notifications');
   const handlePrivacy = () => console.log('Navigate to Privacy');
 
-  
-    return (
-      <View style={styles.container}>
-        <View style={styles.headerContainer}>
-          <Text style={styles.header}>Hello, Clarice</Text>
-          <View style={styles.headerUnderline} />
-        </View>
-        <TouchableOpacity style={styles.menuItem} onPress={() => {}}>
-          <FontAwesome name="history" size={30} color="#000" style={styles.icon} />
-          <Text style={styles.menuText}> History</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem} onPress={() => {}}>
-          <FontAwesome name="bell" size={30} color="#000" style={styles.icon} />
-          <Text style={styles.menuText}>Notifications</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem} onPress={() => {}}>
-          <FontAwesome name="shield" size={30} color="#000" style={styles.icon} />
-          <Text style={styles.menuText}>  Privacy</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem} onPress={() => {}}>
-          <FontAwesome name="sign-out" size={30} color="#000" style={styles.icon} />
-          <Text style={styles.menuText}> Logout</Text>
-        </TouchableOpacity>
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.header}>Hello, Clarice</Text>
+        <View style={styles.headerUnderline} />
       </View>
-    );
+      
+      {/* Sign-up UI */}
+      <View style={styles.authContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Sign-up Email"
+          value={signUpEmail}
+          onChangeText={setSignUpEmail}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Sign-up Password"
+          value={signUpPassword}
+          secureTextEntry
+          onChangeText={setSignUpPassword}
+        />
+        <Button title="Sign Up" onPress={handleSignUpClick} />
+      </View>
+
+      {/* Sign-in UI */}
+      <View style={styles.authContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Sign-in Email"
+          value={signInEmail}
+          onChangeText={setSignInEmail}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Sign-in Password"
+          value={signInPassword}
+          secureTextEntry
+          onChangeText={setSignInPassword}
+        />
+        <Button title="Sign In" onPress={handleSignInClick} />
+      </View>
+
+      {/* Reset Password UI */}
+      <View style={styles.authContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Reset Password Email"
+          value={resetPasswordEmail}
+          onChangeText={setResetPasswordEmail}
+        />
+        <Button title="Reset Password" onPress={handleResetPasswordClick} />
+      </View>
+
+      {/* Sign-out UI */}
+      <Button title="Sign Out" onPress={handleSignOutClick} />
+
+      {/* Display message */}
+      <Text>{message}</Text>
+
+      {/* Other menu items */}
+      <TouchableOpacity style={styles.menuItem} onPress={handleHistory}>
+        <FontAwesome name="history" size={30} color="#000" style={styles.icon} />
+        <Text style={styles.menuText}> History</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.menuItem} onPress={handleNotifications}>
+        <FontAwesome name="bell" size={30} color="#000" style={styles.icon} />
+        <Text style={styles.menuText}>Notifications</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.menuItem} onPress={handlePrivacy}>
+        <FontAwesome name="shield" size={30} color="#000" style={styles.icon} />
+        <Text style={styles.menuText}> Privacy</Text>
+      </TouchableOpacity>
+    </View>
+  );
   };
   
   const styles = StyleSheet.create({
